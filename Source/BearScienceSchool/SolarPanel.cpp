2e80c65713edc9874fe44ff0e99dd2b9e724d0ce
majerok <romanovskii.md@phystech.edu>
2025-10-05 21:28:56 +0300

    Добавлены виджеты для изучения формул. Загружен исходный код солнечных панелей.

---
+++ Source/BearScienceSchool/SolarPanel.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SolarPanel.h"
+
+// Sets default values
+ASolarPanel::ASolarPanel()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CurrentEnergy = 0.f;
+	MaxEnergy = 101.f;
+	ChargeRange = 1.f;
+	isDay = true;
+}
+
+// Called when the game starts or when spawned
+void ASolarPanel::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASolarPanel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (isDay && CurrentEnergy < MaxEnergy) {
+		CurrentEnergy += ChargeRange * DeltaTime;
+		CurrentEnergy = FMath::Clamp(CurrentEnergy, 0.f, MaxEnergy);
+
+		ProgressRange = CurrentEnergy / MaxEnergy;
+	}
+
+}
+
